8bcf84905b4f35f29666240a85e47f51a5f9255e
Dipak Parekh <dipak10494@yahoo.com>
2020-12-19 11:07:21 +0530

    admin-panel workin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CustomerComponent } from './components/customer/customer.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent , data: { title: 'Home' } },
+  { path:'customer', component: CustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
